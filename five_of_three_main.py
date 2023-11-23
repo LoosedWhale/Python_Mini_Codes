4185895c50106dc03ae6d2746a3d4da3cd61ace4
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2023-11-23 17:25:09 +0100

    Create five_of_three_main.py

---
+++ five_of_three_main.py
@@ -0,0 +1,5 @@
+import random
+
+Konsekvens = ["+1", "-1", "+1", "-1", "+2", "-2", "+2", "-2", "+1", "-2", "+3", "Tillbaka till start", "Byt plats med personen till höger", "Byt plats med personen till vänster", "Tillbaka till start", "Instant vinst, så coolt" ]
+
+print(random.choice(Konsekvens))
