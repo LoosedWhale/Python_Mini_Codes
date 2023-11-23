3b057666ef752fb2e996aa62daa5cedb76613498
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2023-11-23 17:32:20 +0100

    Create more_extra.py

---
+++ more_extra.py
@@ -0,0 +1,18 @@
+def lolxd():
+    Svar = input("Vill du svänga höger eller vänster").lower()
+    if Svar == "höger":
+        print("Sväng höger då din dummy")
+    elif Svar == "vänster":
+        print("Sosse sväng vänset då mem du har fel höger är bättre")
+    else:
+        print("da fuck?")
+        lolxd()
+lolxd()
+
+def avdrag():
+    svar = input("Hur långt ifrån ditt jobb bor du? svara i km tack :)")
+    ïf svar >= "50":
+        print("Du har fått avdrag jippy")
+
+
+
